fcbe8c10fc0dd1c1bd94feaab4fe7706c57fd389
Kavindu Theekshana <kavindutheekshana@gmail.com>
2021-09-26 23:56:56 +0530

    mobile home responsive

--- Scripts/main.js
+++ Scripts/main.js
@@ -59,3 +59,97 @@ $(document).ready(function () {
 
 
 });
+
+
+
+
+
+
+
+
+function SplitText(node) {
+  var text = node.nodeValue.replace(/^\s*|\s(?=\s)|\s*$/g, "");
+
+  for (var i = 0; i < text.length; i++) {
+    var letter = document.createElement("span");
+    letter.style.display = "inline-block";
+    letter.style.position = "absolute";
+    letter.appendChild(document.createTextNode(text.charAt(i)));
+    node.parentNode.insertBefore(letter, node);
+
+    var positionRatio = i / (text.length - 1);
+    var textWidth = letter.clientWidth;
+
+    var indent = 100 * positionRatio;
+    var offset = -textWidth * positionRatio;
+    letter.style.left = indent + "%";
+    letter.style.marginLeft = offset + "px";
+  }
+
+  node.parentNode.removeChild(node);
+}
+
+function SplitText2(node) {
+  var text = node.nodeValue.replace(/^\s*|\s(?=\s)|\s*$/g, "");
+
+  for (var i = 0; i < text.length; i++) {
+    var letter = document.createElement("span");
+    letter.style.display = "inline-block";
+    letter.style.position = "absolute";
+    letter.appendChild(document.createTextNode(text.charAt(i)));
+    node.parentNode.insertBefore(letter, node);
+
+    var positionRatio = i / (text.length - 1);
+    var textWidth = letter.clientWidth;
+
+    var indent = 100 * positionRatio;
+    var offset = -textWidth * positionRatio;
+    letter.style.left = indent + "%";
+    letter.style.marginLeft = offset + "px";
+  }
+
+  node.parentNode.removeChild(node);
+}
+
+function SplitText3(node) {
+  var text = node.nodeValue.replace(/^\s*|\s(?=\s)|\s*$/g, "");
+
+  for (var i = 0; i < text.length; i++) {
+    var letter = document.createElement("span");
+    letter.style.display = "inline-block";
+    letter.style.position = "absolute";
+    letter.appendChild(document.createTextNode(text.charAt(i)));
+    node.parentNode.insertBefore(letter, node);
+
+    var positionRatio = i / (text.length - 1);
+    var textWidth = letter.clientWidth;
+
+    var indent = 100 * positionRatio;
+    var offset = -textWidth * positionRatio;
+    letter.style.left = indent + "%";
+    letter.style.marginLeft = offset + "px";
+  }
+
+  node.parentNode.removeChild(node);
+}
+
+function Justify() {
+  var TEXT_NODE = 3;
+  var elem = document.getElementById("character_justify");
+  var elem2 = document.getElementById("character_justify2");
+  var elem3 = document.getElementById("character_justify3");
+  elem = elem.firstChild;
+  elem2 = elem2.firstChild;
+  elem3 = elem3.firstChild;
+
+  while (elem) {
+    var nextElem = elem.nextSibling;
+
+    if (elem.nodeType == TEXT_NODE)
+      SplitText(elem);
+      SplitText2(elem2);
+      SplitText3(elem3);
+
+    elem = nextElem;
+  }
+}
