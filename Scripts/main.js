6d880da3511c8b43838e1fb62f805d70b001ee66
Kavindu Theekshana <kavindutheekshana@gmail.com>
2021-09-26 15:30:02 +0530

    gallery page

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,12 +1,27 @@
 // ----------- Gallery Show and Hide ---------------
-function myFunction() {
+function myGallery() {
     var val = 0;
     var x = document.getElementById('second-menu-gallery');
     if ($( "#second-menu-gallery" ).hasClass( "d-none" )) {
         $("#second-menu-gallery").removeClass("d-none");
+        $("#second-menu-about").addClass("d-none");
+        $("#second-menu-gallery-content").addClass("d-none");
         $("#gallery-menu").addClass("active");
       } else{
         $("#second-menu-gallery").addClass("d-none");
+        $("#second-menu-about").removeClass("d-none");
+        $("#second-menu-gallery-content").removeClass("d-none");
         $("#gallery-menu").removeClass("active");
       }
   }
+
+  // ----------- About --------------- 
+  // function myAbout() {
+  //   var val = 0;
+  //   var x = document.getElementById('second-menu-about');
+  //   if ($( "#second-menu-about" ).hasClass( "d-none" )) {
+  //       $("#second-menu-about").removeClass("d-none");
+  //     } else{
+  //       $("#second-menu-about").addClass("d-none");
+  //     }
+  // }
